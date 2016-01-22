ab39c6e31ef3456937bb8652f4f82cd3ef15a554
nukui-s <nukui.s.aa@gmail.com>
2016-01-22 20:06:08 +0900

    diagtest.py

---
+++ diagtest.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+os.system("rm -rf test")
+
+import tensorflow as tf
+
+x = tf.Variable([1.,2.], name="x")
+diag_x = tf.diag(x)
+loss = tf.reduce_sum(x)
+opt = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+init_op = tf.initialize_all_variables()
+with tf.Session() as sess:
+    sess.run(init_op)
+    sess.run(opt)
